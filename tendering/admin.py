3379729d8f77930d97534ff8789f1fc91f09bc1d
Moham3del <96019073+Moham3del@users.noreply.github.com>
2023-03-21 19:13:29 +0300

    renew all

--- tendering/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(T_detail)
-admin.site.register(owner_list)
-admin.site.register(required1)
-admin.site.register(required)
-admin.site.register(Mngmnt_category)
